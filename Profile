e841fec6daf5e698abaa58d7d85a765364078020
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2026-07-12 15:41:02 +0530

    Add production dependencies

---
+++ Profile
@@ -0,0 +1,1 @@
+web: gunicorn gym_management.wsgi
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import os
 """
 Django settings for config project.
 
@@ -75,11 +77,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
