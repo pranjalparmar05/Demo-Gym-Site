64590c8c46a96076f9108ade063252a9a4b7bfa8
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2026-07-13 22:17:32 +0530

    update URLs

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from django.urls import path, include
 
 # ⚠️ Note: app_name ko abhi hata dete hain taaki direct root matching ho sake
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', include('gym_management.urls')),
+   
     # 📌 Yeh dono paths bilkul exact match hone chahiye:
     path('stripe-checkout/<str:plan_name>/<int:plan_price>/', views.create_checkout_session, name='stripe_checkout'),
     path('activate-plan/<str:plan_name>/', views.activate_plan_view, name='activate_plan'),
