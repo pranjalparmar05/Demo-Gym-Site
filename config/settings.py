bf6771b6f70a1be1460a5f310b6e9fca68000ea6
pranjalparmar05 <pranjalparmar05@gmail.com>
2026-07-17 09:59:20 +0530

    Update gym app configuration in settings
    setings

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'gym',
+    'gym.apps.GymConfig'
     'accounts',
     'users',
     'cloudinary_storage',
